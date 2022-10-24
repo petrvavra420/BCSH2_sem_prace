6d317a9183fc4fbcb90d2864fb83b073a157deb6
PetrVavraMl <petrvavraa@seznam.cz>
2022-10-24 21:05:41 +0200

    InitFin

--- BCSH2_Sem_Prace_Vavra_Petr/MainWindow.xaml.cs
+++ BCSH2_Sem_Prace_Vavra_Petr/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace BCSH2_Sem_Prace_Vavra_Petr
 {
+    //SEM PRACE
     //- obsahuje entity: závod - linky - stroje - mistři - dělníci
     //- každý mistr odpovídá za jednoho nebo více dělníků kteří pracují u strojů, které patří pod
     //- linky a každá linka je umístěna na závodě
@@ -27,7 +28,7 @@ namespace BCSH2_Sem_Prace_Vavra_Petr
     {
         public MainWindow()
         {
-            //test
+            
             InitializeComponent();
             DatabaseTools dt = new DatabaseTools();
             LiteDatabase db = dt.connectToDatabase(@"C:\Temp\DatabaseTest.db");
